e0725e64f731c83467c87a72d3839a0fa696df45
miladimos <milad.jafari6210@gmail.com>
2020-11-26 15:43:32 +0330

    Add php

---
+++ php/php.php
@@ -0,0 +1,13 @@
+// Variables
+
+$num = 10;
+$pi  = 3.14;
+$str = 'This is String';
+$b   = true;
+$arr = [1,2,3,4];
+$n   = null;
+
+echo '$num = '. $n;
+echo ' and its type is ' . gettype($n) . '.<br>';
+
+
