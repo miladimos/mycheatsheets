fd95906bbb9e8cdea0e81779313abd5e1664830d
milad.jafari6210@gmail.com <milad.jafari6210@gmail.com>
2021-03-05 19:10:51 +0330

    edit php string

--- php/string.php
+++ php/string.php
@@ -45,6 +45,14 @@ $c = 'b';
 // echo strrev($str) . '<br >';
 
 // echo strpos($str, 'o') . '<br >';
+// echo strrpos($str, 'o') . '<br >';
+// echo stripos($str, 'o') . '<br >';
+// echo strripos($str, 'o') . '<br >';
+
+trim($str);
+rtrim($str);
+ltrim($str);
+
 
 $firstSpace = strpos($str, ' ');
 $secondSpace = strpos($str, ' ', $firstSpace + 1);
@@ -53,6 +61,21 @@ $secondSpace = strpos($str, ' ', $firstSpace + 1);
 // echo substr($str, 2) . '<br >';
 // echo substr($str, 3, -5) . '<br >';
 
+str_replace("is", "was", $str);
+str_replace("is", "was", $str);
+
+
+substr_count($str, 'rl');
+substr_count($str, 'rl', 3);
+substr_count($str, 'rl', 3, 5);
+
+substr_replace($str, 'new', 0);
+substr_replace($str, 'new', 0, 10);
+substr_replace($str, 'new', 0, 0);
+substr_replace($str, 'new', 3, 0);
+
+
+str_repeat('-', 10);
 
 // printf('This is str: %s', $str);
 // $r = sprintf('This is str: %s', $str);
@@ -66,3 +89,34 @@ $secondSpace = strpos($str, ' ', $firstSpace + 1);
 // print_r($res);
 // echo '</pre>';
 
+
+$html = "<p>this is test html</p>";
+htmlspecialchars($html);
+/*  Second arg: ENT_COMPAT, ENT_QUOTES, ENT_NOQUOTES */
+
+// htmlentities($html); 
+
+strip_tags($html); // delete html tags from string
+// seconda args: tags that do not removed
+
+
+$url = 'http:://google.com';
+urlencode($url);
+urldecode($url);
+
+rawurlencode($url);
+rawurldecode($url);
+
+// just different between urlencode and rawurlencode is space
+
+$path = "PHP is cool";
+$pathEncoded = urlencode($path);
+$url = "https://google.com/?q=".$pathEncoded;
+
+
+$s = 'd,d2,d3';
+explode(',', $s);
+explode(',', $s, 2);
+
+$a= ["c", "c1", "c2"];
+implode(',', $a);
