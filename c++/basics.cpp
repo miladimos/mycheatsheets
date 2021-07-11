c294c6c5d98249532aaad21609149e49577fd07d
miladimos <milad.jafari6210@gmail.com>
2021-07-11 04:40:01 +0430

    add cpp files

--- c++/basics.cpp
+++ c++/basics.cpp
@@ -5,27 +5,27 @@
 
 using namespace std;
 
-int multiple(int ); // prototype
+int multiple(int); // prototype
 
 void swap(int &, int &);
 
 int strlength(const char *);
 void strconcat(char *, const char *);
 
-#define min(x,y) (x<y ? x : y) // macro
+#define min(x, y) (x < y ? x : y) // macro
 
-template < class T >
+template <class T>
 T maximum(T x, T y)
 {
     T max;
     if (x > y)
         max = x;
-    else 
+    else
         max = y;
     return max;
 }
 
-int main() 
+int main()
 {
     // cout<<"Hello"<<endl<<"World";
     // cerr << "\nError";
@@ -38,7 +38,7 @@ int main()
 
     // char c; // -128...127
     // unsigned char c; // 0...255
-    // c = 'a'; // a: 97 A: 65 
+    // c = 'a'; // a: 97 A: 65
 
     // int a;
     // a++ first assign and increment
@@ -52,10 +52,9 @@ int main()
     // cout.setf(ios::left); // ios::right
     // cout<<'a';
 
-
     // char ch;
     // int x;
-    // for(ch='a'; ch<='z'; ch++) 
+    // for(ch='a'; ch<='z'; ch++)
     // {
     //     x=ch;
     //     cout<<ch<<"="<<x<<endl;
@@ -82,31 +81,26 @@ int main()
 
     // int a[3]={1,2,3};
 
-
     // int a=4;
     // int *pa;
     // pa=&a;
     // cout<<*pa<<endl; // 4
     // cout<<sizeof(pa)<<endl; // 8 byte
-    
 
     // int a=3;
     // const int *pa=&a;
     // *pa=2;   // Error
     // pa = &b; // Ok
 
-
     // int b=3;
     // int * const pb=&b;
     // *pb=2;    // Ok
     //  pb = &c; // Error
 
-    
     // int c=3;
     // const int * const pc=&c;
     // *pc=2;    // Error
     //  pc = &d; // Error
-
 
     // pointer to pointer
 
@@ -143,22 +137,20 @@ int main()
     //         break;
     // }
 
-
     // char s1[] = "10";
-    // cout<< atoi(s1); 
+    // cout<< atoi(s1);
 
     // char s2[] = "1.5";
-    // cout<< atof(s2); 
+    // cout<< atof(s2);
 
     // char ch;
     // while(cin.get(ch))
     // {
     //     if(islower(ch))
     //         cout<<char(toupper(ch));
-    //     else 
+    //     else
     //         cout<<ch;
     // }
-
 
     // char ch;
     // int ca = 0, cd = 0, can= 0;
@@ -218,7 +210,6 @@ int main()
     // cout << strspn(s1, s2); // 2
     // cout << strcspn(s1, s2); // 01
 
-
     // char s[] = "abcde";
     // char s1[] = "efg";
     // cout<<static_cast<char *> (memset(s, 'g', 3)); // gggde
@@ -235,8 +226,8 @@ int main()
     // cout<<s.length();
     // cout<<s.find('e');
     // cout<<s.replace(1,3, "gg"); // Hggo
-    // s1.assign(s); 
-    // s1.append(s); 
+    // s1.assign(s);
+    // s1.append(s);
     // cout<<s1;
     // cout<<s.substr(2, 2); // ll
     // s.erase(3,2);
@@ -246,7 +237,6 @@ int main()
     // k = s.find("e");
     // s.replace(k+1, 0, "dd");
     // cout<<s; // Heddllo
-
 
     // string s("this is str ");
     // int p;
@@ -266,17 +256,83 @@ int main()
 
     // char *ars[3]={"apple", "orange", "banana"};
 
+    // enum Color
+    // {
+    //     RED,
+    //     GREEN,
+    //     BLUE
+    // };
+    // enum sign
+    // {
+    //     neg = -1,
+    //     zero = 0,
+    //     pos = 1
+    // };
 
-    enum Color {RED, GREEN, BLUE};
-    enum sign {neg = -1, zero = 0, pos= 1};
+    // typedef enum
+    // {
+    //     a,
+    //     b,
+    //     c
+    // } x;
+    // x v1, v2;
+    // v1 = a;
+    // cout << v1;
 
-    
+    // struct point {
+    //     int x;
+    //     int y;
+    // };
+
+    // point p = {10, 10};
+
+
+    // struct students{
+    //     char name[20];
+    //     int grades[2];
+    // } s[2];
+
+    // strcpy(s[0].name, "ali");
+    // strcpy(s[1].name, "john");
+
+    // struct Time {
+    //     int h,m,s;
+    // };
+    // Time t1,t2;
+    // t1.h=7;
+    // t1.m=0;
+    // t1.s=0;
+    // Time *p; // pointer to struct
+    // p = &t1;
+    // cout<<p->h<<endl;
+    // cout<<(*p).h;
+
+    // struct s {
+    //     double a;
+    //     void f(int x) {a=x;}
+    //     int  g(void) {int y; y=a*a; return (y);}
+    // } m;
+    // m.f(4);
+    // cout<<m.g(); //16
+
+    // struct a{
+    //     long c;
+    //     union d{
+    //         int e;
+    //         char f;
+    //     } u;
+    // }s;
+    // s.c=100;
+    // s.u.e=65;
+    // cout<<s.u.f; // A
+
+
     // getchar();
 }
 
 int multiple(int x)
 {
-    return x*2;
+    return x * 2;
 }
 
 void swap(int &a, int &b)
@@ -287,31 +343,32 @@ void swap(int &a, int &b)
     b = temp;
 }
 
-
 int strlength(const char *s)
 {
-    int c=0;
-    for(; *s!='\0'; s++)
+    int c = 0;
+    for (; *s != '\0'; s++)
         c++;
     return c;
 }
 
 void strcopy(char *a, const char *b)
 {
-    for(; (*a=*b) != '\0'; a++, b++);
+    for (; (*a = *b) != '\0'; a++, b++)
+        ;
 }
 
 void strcopy2(char *a, const char *b)
 {
-    int i=0;
-    for(; (a[i]=b[i]) != '\0'; i++);
+    int i = 0;
+    for (; (a[i] = b[i]) != '\0'; i++)
+        ;
 }
 
 void strconcat(char *a, const char *b)
 {
-    while(*a != '\0')
+    while (*a != '\0')
         ++a;
 
-    for(; *a=*b; a++, b++);
+    for (; *a = *b; a++, b++)
+        ;
 }
-
---
+++ c++/files.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    /*
+        END OF FILE (text = 1A or 26) (binary = ctrl+z)
+        END OF LINE (text = CR/LF)
+    */
+
+//    ifstream f1;
+//    f1.open('a.dat', ios::in);
+//    f1.close();
+//    ifstream f2("a.dat", ios::in);
+
+    // ofstream f2;
+    // f2.open("a.dat", ios::out);
+
+    // fstream f3("a.dat", ios::in | ios::out) // ios::app, ios::ate, ios::binary, ios::trunc, 
+
+    // eof(); // is end of file?
+
+
+    // char ch;
+    // ofstream f1("a.dat"); // ios::binary
+    // if(!f1)
+    //     exit(0);
+    // cout<<"Enter message:\n";
+    // do{
+    //     cin.get(ch);
+    //     f1.put(ch);
+    // } while(ch != '$');
+    // f1.close();
+
+    // cout<<"output:\n";
+    // ifstream f2("a.dat");
+    // f2.get(ch);
+    // while(!f2.eof())
+    // {
+    //     cout.put(ch);
+    //     f2.get(ch);
+    // }
+
+    // f2.close();
+
+
+}
